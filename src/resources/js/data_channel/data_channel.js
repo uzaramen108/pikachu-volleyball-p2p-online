48b1404864fc7fb60c66f8082d9dcc1f80adbf95
uzaramen108 <parkgysung010845@gmail.com>
2025-10-23 23:02:13 +0900

    Update nickname display function for peers
    Replaced call to displayNicknameFor with displayPeerNicknameFor for filtering peer's nickname.

--- src/resources/js/data_channel/data_channel.js
+++ src/resources/js/data_channel/data_channel.js
@@ -51,6 +51,7 @@ import {
 import {
   displayMyAndPeerNicknameShownOrHidden,
   displayNicknameFor,
+  displayPeerNicknameFor,
   displayPartialIPFor,
 } from '../nickname_display.js';
 import {
@@ -480,7 +481,8 @@ function receiveChatMessageFromPeer(chatMessage) {
         .slice(0, -1)
         .trim()
         .slice(0, MAX_NICKNAME_LENGTH);
-      displayNicknameFor(channel.peerNickname, channel.amICreatedRoom);
+      displayPeerNicknameFor(channel.peerNickname, channel.amICreatedRoom); 
+      // Replaced function for filtering peer's nickname
       displayNicknameFor(channel.myNickname, !channel.amICreatedRoom);
       displayPartialIPFor(channel.peerPartialPublicIP, channel.amICreatedRoom);
       displayPartialIPFor(channel.myPartialPublicIP, !channel.amICreatedRoom);
